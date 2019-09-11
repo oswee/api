3590202a1cff939591e2b35c89bbb309105d5939
Dzintars <dzintars.klavins@gmail.com>
2019-09-11 19:27:33 +0300

    Generate entities

--- oswee/identity/signup/proto/v1/protoc-gen.sh
+++ oswee/identity/signup/proto/v1/protoc-gen.sh
@@ -5,6 +5,24 @@
 protoc \
     --proto_path=. \
     --go_out=plugins=grpc:. \
+    oswee/identity/signup/proto/v1/signup.proto
+
+protoc \
+    --proto_path=. \
+    --grpc-gateway_out=logtostderr=true:. \
+    oswee/identity/signup/proto/v1/signup.proto
+
+protoc \
+    --proto_path=. \
+    --proto_path=oswee/identity/signup/proto/v1 \
+    --swagger_out=logtostderr=true:oswee/identity/signup/swagger/v1 \
+    signup.proto
+
+# Command API
+
+protoc \
+    --proto_path=. \
+    --go_out=plugins=grpc:. \
     oswee/identity/signup/proto/v1/signup_command_api.proto
 
 protoc \

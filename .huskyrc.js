bba9c999b0e8b2f53b4fb8d6413768d527c29f57
Dzintars Klavins <dzintars.klavins@gmail.com>
2020-08-31 19:18:35 +0300

    feat(module): test package publishing

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,7 +2,8 @@ const tasks = arr => arr.join(' && ')
 
 module.exports = {
   hooks: {
-    'pre-commit': tasks(['lint-staged']),
+    // 'pre-commit': tasks(['lint-staged']),
+    "pre-commit": "lerna run --concurrency 1 --stream precommit",
     'commit-msg': tasks(['commitlint -E HUSKY_GIT_PARAMS']),
   },
 }
